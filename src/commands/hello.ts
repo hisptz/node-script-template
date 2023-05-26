3a9005da8534937ed122c7c95777e463f283cda2
gian <nnkogift@hotmail.com>
2023-05-26 14:10:53 +0300

    feat(logging): Implementation
    - Setup winston for logging

--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -1,5 +1,6 @@
 import {Command} from "commander";
 import appDetails from "../../package.json"
+import logger from "../logging";
 
 const helloProgram = new Command();
 
@@ -8,7 +9,7 @@ helloProgram.name("hello-dhis2-script").description("An example program. Delete 
 helloProgram.command("say-hi").option("-n --name <name>", "The name of the user").action((args) => {
     const {name} = args ?? {}
     //Here you can call any of your functions to do what is necessary. Use the args to access the argument object as specified on the options
-    console.log(`Hello ${name}, Welcome to the dhis2-node-script`)
+    logger.info(`Hello ${name}, Welcome to the dhis2-node-script`)
 })
 
 export {
